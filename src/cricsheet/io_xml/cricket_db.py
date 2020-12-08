bc095fb7e09a13cf6017d9cc06019e9bd6e2c5d0
AG <mad_ad90@hotmail.com>
2020-12-08 23:02:04 +0000

    update path in xml parser; remove copies of yaml code

--- src/cricsheet/io_xml/cricket_db.py
+++ src/cricsheet/io_xml/cricket_db.py
@@ -13,20 +13,20 @@ logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
 
 #if __name__ == '__main__':
-engine = create_engine('sqlite:///cricsheet_xml_v3.db', echo=False)
+engine = create_engine('sqlite:///cricsheet_xml.db', echo=False)
 Session = sessionmaker(bind=engine)
 session = Session()
 Base.metadata.create_all(engine)
 reader = CricsheetXMLReader()
 
-objects = reader.get_objects_from_directory('data/raw/data')
+objects = reader.get_objects_from_directory('data/raw/xml/data')
 #objects = reader.get_objects_from_directory('data/test/')
 
 end_time = time.time()
 print(f'{end_time - start_time} seconds')
 
 #%%
-import pickle
+i#mport pickle
 
 #pickle.dump(objects, open( "parsedXML.p", "wb" ))
 #objects = pickle.load( open( "parsedXML.p", "rb" ) )
