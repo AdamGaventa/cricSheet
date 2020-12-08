75a400c5c9a7b6dfc3a2b65817df7e9aa7c555c5
Adam Gaventa <32434438+AdamGaventa@users.noreply.github.com>
2020-12-08 22:25:21 +0000

    dev (#1)
    * yaml parser and create db for match metadata
    
    * dummy db script; run main script with 2020 data
    
    * restart using pyscaffold project structure (#2)
    
    * restart using pyscaffold project structure
    
    * move git repository
    
    * successfully installed, so moved Match class back to separate module
    
    * update readme
    
    * start ball-by-ball
    
    * clean up

---
+++ src/cricsheet/temp_createDb.py
@@ -0,0 +1,30 @@
+"""
+Testing sqlalchemy with dummy db
+"""
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+
+Base = declarative_base()
+
+class TestUser(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+
+engine = create_engine('sqlite:///Data/sqlite/dummy.db', echo=True)
+Base.metadata.create_all(bind=engine)
+Session = sessionmaker(bind=engine)
+
+session = Session()
+
+user = TestUser()
+user.id = 0
+user.username = 'JoeBloggs'
+
+session.add(user)
+session.commit()
+
+
+session.close()
